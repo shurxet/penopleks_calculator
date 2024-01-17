54c91450275091603dd4fa3cdb061e058f4f54f0
shurxet <s.shurupov.88@mail.ru>
2024-01-17 19:46:42 +0300

    updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DJANGO_SETTINGS_MODULE = env('DJANGO_SETTINGS_MODULE')
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = ['https://instok.ru ']
